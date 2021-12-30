0f0e5d7184f5da53481d6b7153c9818ef915697d
E-K8 <ekatplat@gmail.com>
2021-12-30 21:42:17 +0000

    re-add db.js

---
+++ db.js
@@ -0,0 +1,13 @@
+import pg from "pg";
+
+const pool = new pg.Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "students",
+  password: "postrespassword", // TODO change to your postreSQL password
+  port: 5432,
+});
+
+export default pool;
+
+// original tutorial uses const Pool = require("pg").Pool; on line 1 and module.exports = pool; on line 11 instead of export default....
